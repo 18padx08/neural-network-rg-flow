3707e71aece398b8f6befa298b5664ecd95e0dd2
amrein <amrein@ubique.ch>
2018-02-15 16:16:55 +0100

    changed pointers to vectors following https://stackoverflow.com/questions/381621/using-arrays-or-stdvectors-in-c-whats-the-performance-gap arguments

--- RBMCPU/TIRBM.cpp
+++ RBMCPU/TIRBM.cpp
@@ -4,7 +4,7 @@
 
 TIRBM::~TIRBM()
 {
-	//deallocate every used memory
+
 }
 
 void TIRBM::initWeights()
@@ -15,19 +15,23 @@ void TIRBM::setParameters(ParamSet set)
 {
 }
 
-void TIRBM::train(double ** input, int sample_size, int epoch)
+void TIRBM::train(vector<vector<double>>& input, int sample_size, int epoch)
 {
 }
+
+
 
 double * TIRBM::sample_from_net(int gibbs_steps)
 {
 	return nullptr;
 }
 
-double * TIRBM::reconstruct(double * input, int gibbs_steps)
+double * TIRBM::reconstruct(vector<double>& input, int gibbs_steps)
 {
 	return nullptr;
 }
+
+
 
 void TIRBM::saveToFile(std::string filename)
 {
@@ -42,13 +46,14 @@ bool TIRBM::loadWeights(std::string filename)
 	return false;
 }
 
-void TIRBM::propagate_down(double ** input, double ** output, int sample_size)
+void TIRBM::propagate_down(vector<vector<double>>& input, vector<vector<double>>& output, int sample_size)
 {
 }
 
-void TIRBM::propagate_up(double ** input, double ** output, int gibbs_steps)
+void TIRBM::propagate_up(vector<vector<double>>& input, vector<vector<double>>& output, int gibbs_steps)
 {
 }
+
 
 double TIRBM::bernoulli(double p)
 {
@@ -60,18 +65,20 @@ double TIRBM::uniform(double min, double max)
 	return 0.0;
 }
 
-void TIRBM::sample_h_given_v(double * vis_src, double * hid_target, double * hid_target_sample)
+void TIRBM::sample_h_given_v(vector<double>& vis_src, vector<double>& hid_target, vector<double>& hid_target_sample)
 {
 }
 
-void TIRBM::sample_v_given_h(double * hid_src, double * vis_target, double * vis_target_sample)
+void TIRBM::sample_v_given_h(vector<double>& hid_src, vector<double>& vis_target, vector<double>& vis_target_sample)
 {
 }
 
-double TIRBM::contrastive_divergence(double ** input, int cdK, int batchSize)
+double TIRBM::contrastive_divergence(vector<vector<double>>& input, int cdK, int batchSize)
 {
 	return 0.0;
 }
+
+
 
 TIRBM::TIRBM(int n_vis, int n_hid, FunctionType activationFunction) : generator(time(NULL)), distribution(0.0,1.0), actFun(activationFunction)
 {
--- RBMCPU/TIRBM.h
+++ RBMCPU/TIRBM.h
@@ -6,22 +6,23 @@
 #include <ctime>
 #include <random>
 #include "RBM.h"
+using namespace std;
 class TIRBM
 {
 private:
-	std::vector<Symmetry<int>> symmetries;
+	vector<Symmetry<int>> symmetries;
 	//weights
-	double **wij;
+	vector<vector<double>> wij;
 	//last changes for the weights (e.g. used for momentum)
-	double **dWij;
+	vector<vector<double>> dWij;
 	//use for temporary changes to assure thread safty with parallel programming
-	double **tmpDwij;
+	vector<vector<double>> tmpDwij;
 	//hidden bias matrix
-	double **bjs;
-	double **dbjs;
+	vector<vector<double>> bjs;
+	vector<vector<double>> dbjs;
 	//visible biases 
-	double *ci;
-	double *dci;
+	vector<double> ci;
+	vector<double> dci;
 
 	//parameters used for learning
 	ParamSet parameters;
@@ -34,11 +35,11 @@ private:
 	//sample from unifrom dist
 	double uniform(double min, double max);
 	//positive step
-	void sample_h_given_v(double *vis_src, double *hid_target, double *hid_target_sample);
+	void sample_h_given_v(vector<double> &vis_src, vector<double> &hid_target, vector<double> &hid_target_sample);
 	//negative step
-	void sample_v_given_h(double *hid_src, double *vis_target, double *vis_target_sample);
+	void sample_v_given_h(vector<double> &hid_src, vector<double> &vis_target, vector<double> &vis_target_sample);
 	//approximate the gradient for the log likelyhood
-	double contrastive_divergence(double **input, int cdK, int batchSize);
+	double contrastive_divergence(vector<vector<double>> &input, int cdK, int batchSize);
 	ActivationFunction actFun;
 
 public:
@@ -50,11 +51,11 @@ public:
 	//set the parameters for the learning algorithm such as learning rate, momentum, activationFunction, regularization
 	void setParameters(ParamSet set);
 	//train for number of epochs with a test set
-	void train(double **input, int sample_size, int epoch);
+	void train(vector<vector<double>> &input, int sample_size, int epoch);
 	//sample from the net using gibbs_steps of gibbs sampling iterations
 	double * sample_from_net(int gibbs_steps = 10);
 	//reconstruct the data using a fixed number of gibbs iterations
-	double * reconstruct(double * input, int gibbs_steps = 1);
+	double * reconstruct(vector<double> &input, int gibbs_steps = 1);
 	//save the state of the net to a file
 	void saveToFile(std::string filename);
 	//save a visualization of different parameters for visible check of algorithm
@@ -62,8 +63,8 @@ public:
 	//load a previously saved state
 	bool loadWeights(std::string filename);
 	//helper function for dbms (propagate to next layer)
-	void propagate_down(double **input,double **output, int sample_size);
+	void propagate_down(vector<vector<double>> &input, vector<vector<double>> &output, int sample_size);
 	//helper function for dbms (propagate to previous layer)
-	void propagate_up(double **input, double **output,  int gibbs_steps = 10);
+	void propagate_up(vector<vector<double>> &input, vector<vector<double>> &output,  int gibbs_steps = 10);
 };
 
